e74c1cfb6d9d8ac3f1c72daffd870d41cbc1fecd
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2023-08-30 16:28:13 -0300

    :tada: Commit inicial

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Create Next App',
@@ -10,8 +10,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
