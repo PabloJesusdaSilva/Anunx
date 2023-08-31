5e953816fb0ecbc0c5b2ad3a4290e44702cac3b5
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2023-08-31 12:44:49 -0300

    :fix: Corrindo erro de responsividade

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 const Header = () => {
   return (
-    <header className="flex justify-center w-full h-20 bg-zinc-900">
+    <header className="flex justify-center w-full h-16 bg-zinc-900">
       <nav className=" flex items-center p-2">
-        <ul className="grid grid-cols-2 items-center gap-x-96">
+        <ul className="grid grid-cols-2 items-center md:gap-x-96 sm:gap-28">
           <li className="text-lg text-white">Anux</li>
           <li>
             <button className="py-1 px-4 bg-zinc-900 border-2 rounded-md border-zinc-700 text-white">
