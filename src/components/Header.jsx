631e4600aac05c97f173e7bbe86b7d7ce29bcc8d
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2023-09-01 16:03:43 -0300

    :fix: Corrigindo orgnização das pastas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ const Header = () => {
     <header className="flex justify-center w-full h-16 bg-zinc-900">
       <nav className=" flex items-center p-2">
         <ul className="grid grid-cols-2 items-center md:gap-x-96 sm:gap-28">
-          <li className="text-lg text-white">Anux</li>
+          <li className="text-lg text-white">Anunx</li>
           <li>
             <button className="py-1 px-4 bg-zinc-900 border-2 rounded-md border-zinc-700 text-white">
               Anunciar e Vender
--- src/templates/Default.jsx
+++ src/templates/Default.jsx
@@ -1,11 +1,10 @@
-import Header from "../components/Header";
+import Header from '../components/Header';
 
 const Default = ({ children }) => {
   return(
     <>
       <Header />
         {children}
-      <footer></footer>
     </>
   );
 }
