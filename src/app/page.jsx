2dda78a0101f7d2237323c3b36c4165fb1e49f1c
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2023-09-01 16:04:15 -0300

    :feat: Criando página para gerar anúncios

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,13 @@
 import Image from 'next/image';
 
-import TemplateDefault from '@/templates/Default';
-
 import localImage from '../../public/bike.jpg';
 
 const Home = () => {
   return (
-    <TemplateDefault>
       <main className='flex flex-col items-center w-full mt-14'>
         <h1 className='text-5xl'>Meus Anúncios</h1>
 
-        <button className='mt-8 py-2 px-5 rounded-md text-md uppercase text-white bg-zinc-950'>
+        <button className='mt-8 py-2 px-5 rounded-md uppercase text-white bg-zinc-950'>
           Publicar novo anúncio
         </button>
 
@@ -18,7 +15,7 @@ const Home = () => {
           <article>
             <Image className='rounded-t-md' width={200} height={100} src={localImage} alt='foto do produto à venda' />
 
-            <div className='grid grid-cols-2 p-3 shadow-xl'>
+            <div className='grid grid-cols-2 p-3 shadow-xl bg-white rounded-b-md'>
               <h4 className='col-span-2 text-lg font-bold '>Produto x</h4>
               <span className='col-span-2 text-sm '>R$60,00</span>
 
@@ -29,7 +26,7 @@ const Home = () => {
           <article>
             <Image className='rounded-t-md' width={200} height={100} src={localImage} alt='foto do produto à venda' />
 
-            <div className='grid grid-cols-2 p-3 shadow-xl'>
+            <div className='grid grid-cols-2 p-3 shadow-xl bg-white rounded-b-md'>
               <h4 className='col-span-2 text-lg font-bold '>Produto x</h4>
               <span className='col-span-2 text-sm '>R$60,00</span>
 
@@ -40,7 +37,7 @@ const Home = () => {
           <article>
             <Image className='rounded-t-md' width={200} height={100} src={localImage} alt='foto do produto à venda' />
 
-            <div className='grid grid-cols-2 p-3 shadow-xl'>
+            <div className='grid grid-cols-2 p-3 shadow-xl bg-white rounded-b-md'>
               <h4 className='col-span-2 text-lg font-bold '>Produto x</h4>
               <span className='col-span-2 text-sm '>R$60,00</span>
 
@@ -50,7 +47,6 @@ const Home = () => {
           </article>
         </div>
       </main>
-    </TemplateDefault>
   );
 }
 
---
+++ src/app/publish/page.jsx
@@ -0,0 +1,60 @@
+const Publish = () => {
+  return (
+      <main className='flex flex-col items-center w-full mt-12 mb-10'>
+        <h2 className='text-5xl'>
+          Publicar Anúncio
+        </h2>
+        <h5 className='mt-2 text-lg'>
+          Quanto mais detalhado, melhor!
+        </h5>
+
+        <div className='w-6/12 mt-10 bg-white px-4 py-6 rounded-md shadow-lg'>
+          <h6 className='mb-1 text-lg'>Título do Anúncio</h6>
+          <input className='w-full p-1 border-b-2 border-gray-200' type='text' placeholder='Ex.: Bike aro 18 com garantia' />
+
+          <h6 className='mt-6 mb-1 text-lg'>Categoria</h6>
+          <select className='w-full py-2 text-gray-400 border-b-2 border-gray-200' name='category'>
+            <option value='0'>Selecione</option>
+            <option value='1'>Bebê e Criança</option>
+            <option value='2'>Agricultura</option>
+            <option value='3'>Moda</option>
+            <option value='4'>Carros, Motos e Barcos</option>
+            <option value='5'>Serviços</option>
+            <option value='6'>Lazer</option>
+            <option value='7'>Animais</option>
+            <option value='7'>Moveis, Casas, Jardim</option>
+            <option value='7'>Imóveis</option>
+            <option value='7'>Equipamentos e Ferramentas</option>
+            <option value='7'>Celulares e Tablets</option>
+            <option value='7'>Esportes</option>
+            <option value='7'>Tecnologia</option>
+            <option value='7'>Emprego</option>
+            <option value='7'>Outros</option>
+          </select>
+        </div>
+
+        <div className='w-6/12 mt-10 bg-white px-4 py-6 rounded-md shadow-lg'>
+          <h6 className='mb-3 text-lg'>Imagens</h6>
+          <p className='text-sm text-gray-600'>A primeira imagem é a foto principal do seu anúncio</p>
+        </div>
+
+        <div className='w-6/12 mt-10 px-4 py-6 rounded-md shadow-lg bg-white '>
+          <h6 className='mb-3 text-lg'>Descrição</h6>
+          <textarea className='w-full border-2 border-gray-200 rounded resize-none' name='description' rows='6'></textarea>
+        </div>
+
+        <div className='w-6/12 mt-10 px-4 py-6 rounded-md shadow-lg bg-white'>
+          <h6 className='mb-3 text-lg'>Dados de Contanto</h6>
+          <input className='w-full p-2 border-2 border-gray-200 rounded' type="email" placeholder="Email"/>
+          <input className='w-full my-3 p-2 border-2 border-gray-200 rounded' type="text" placeholder="Nome"/>
+          <input className='w-full p-2 border-2 border-gray-200 rounded' type="tel" placeholder="Telefone"/>
+        </div>
+
+        <div className='w-6/12 flex justify-end mt-6'>
+          <button className='py-2 px-5 rounded-md uppercase bg-zinc-950 text-white'>Publicar anúncio</button>
+        </div>
+      </main>
+  );
+}
+
+export default Publish;
