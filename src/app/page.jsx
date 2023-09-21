04bfd1b7bca174da8b3173845344e1874309e048
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2023-09-21 10:23:45 -0300

    :feat: Criando página da home deslogada e ajustando dashboard

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,26 +1,39 @@
 import Image from 'next/image';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 import localImage from '../../public/bike.jpg';
 
 const Home = () => {
   return (
-      <main className='flex flex-col items-center w-full mt-14'>
-        <h1 className='text-5xl'>Meus Anúncios</h1>
+    <main className='flex flex-col items-center w-full'>
+      <h1 className='mt-10 mb-6 text-4xl'>O que deseja encontrar?</h1>
+      <div className='w-[600px] h-12'>
+         <input 
+            type='text'
+            placeholder='Ex: iPhone 12 com garantia'
+            className='w-full h-full pl-4 rounded-md shadow-md'
+         />
+         <MagnifyingGlassIcon className='relative -top-9 -right-[550px] w-6 ' />
+      </div>
 
-        <button className='mt-8 py-2 px-5 rounded-md uppercase text-white bg-zinc-950'>
-          Publicar novo anúncio
-        </button>
+      <section className='flex flex-col items-center mt-14'>
+        <h2 className='mb-3 text-3xl'>Destaques</h2>
 
-        <div className='grid sm:grid-cols-1 md:grid-cols-3 gap-x-6 mt-16 tracking-wide'>
+        <div className='flex gap-6'>
+          <article>
+              <Image className='rounded-t-md' width={200} height={100} src={localImage} alt='foto do produto à venda' />
+
+              <div className='grid grid-cols-2 p-3 shadow-xl bg-white rounded-b-md'>
+                <h4 className='col-span-2 text-lg font-bold '>Produto x</h4>
+                <span className='col-span-2 text-sm '>R$60,00</span>
+              </div>
+          </article>
           <article>
             <Image className='rounded-t-md' width={200} height={100} src={localImage} alt='foto do produto à venda' />
 
             <div className='grid grid-cols-2 p-3 shadow-xl bg-white rounded-b-md'>
               <h4 className='col-span-2 text-lg font-bold '>Produto x</h4>
               <span className='col-span-2 text-sm '>R$60,00</span>
-
-              <button className='col-span-1 -ml-11 mt-5'>Editar</button>
-              <button className='mt-5'>Remover</button>
             </div>
           </article>
           <article>
@@ -29,24 +42,11 @@ const Home = () => {
             <div className='grid grid-cols-2 p-3 shadow-xl bg-white rounded-b-md'>
               <h4 className='col-span-2 text-lg font-bold '>Produto x</h4>
               <span className='col-span-2 text-sm '>R$60,00</span>
-
-              <button className='col-span-1 -ml-11 mt-5'>Editar</button>
-              <button className='mt-5'>Remover</button>
-            </div>
-          </article>
-          <article>
-            <Image className='rounded-t-md' width={200} height={100} src={localImage} alt='foto do produto à venda' />
-
-            <div className='grid grid-cols-2 p-3 shadow-xl bg-white rounded-b-md'>
-              <h4 className='col-span-2 text-lg font-bold '>Produto x</h4>
-              <span className='col-span-2 text-sm '>R$60,00</span>
-
-              <button className='col-span-1 -ml-11 mt-5'>Editar</button>
-              <button className='mt-5'>Remover</button>
             </div>
           </article>
         </div>
-      </main>
+      </section>
+   </main>
   );
 }
 
