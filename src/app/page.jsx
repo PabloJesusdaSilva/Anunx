aefe83df4955fdfc5fc89e16be53fbc186a7f57b
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2023-09-21 12:15:47 -0300

    :fix: Ajustando espaçamentos da página principal e do header

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,20 +6,20 @@ import localImage from '../../public/bike.jpg';
 const Home = () => {
   return (
     <main className='flex flex-col items-center w-full'>
-      <h1 className='mt-10 mb-6 text-4xl'>O que deseja encontrar?</h1>
-      <div className='w-[600px] h-12'>
+      <h1 className='sm:text-3xl mt-6 md:mt-10 mb-6 text-4xl'>O que deseja encontrar?</h1>
+      <div className='sm: w-[330px] md:w-[600px] h-12'>
          <input 
             type='text'
             placeholder='Ex: iPhone 12 com garantia'
             className='w-full h-full pl-4 rounded-md shadow-md'
          />
-         <MagnifyingGlassIcon className='relative -top-9 -right-[550px] w-6 ' />
+         <MagnifyingGlassIcon className='relative -top-9 -right-[550px] w-6 text-zinc-600' />
       </div>
 
       <section className='flex flex-col items-center mt-14'>
-        <h2 className='mb-3 text-3xl'>Destaques</h2>
+        <h2 className='text-3xl'>Destaques</h2>
 
-        <div className='flex gap-6'>
+        <div className='grid sm:grid-cols-1 gap-y-8 md:grid-cols-3 gap-x-6 mt-6 tracking-wide'>
           <article>
               <Image className='rounded-t-md' width={200} height={100} src={localImage} alt='foto do produto à venda' />
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,11 +14,11 @@ const Header = () => {
   return (
     <header className='flex justify-center w-full h-16 shadow-md shadow-gray-400 bg-zinc-900'>
       <nav className=' flex items-center p-2'>
-        <ul className='grid grid-cols-2 items-center md:gap-x-96 sm:gap-28'>
+        <ul className='grid grid-cols-2 items-center gap-x-96'>
           <li className='text-lg text-white'>Anunx</li>
 
           <li className='flex items-center'>
-            <Link href='../publish' passHref className='mr-4'>
+            <Link href='../publish' passHref className='mr-6'>
               <button className='py-1 px-4 bg-zinc-900 border-2 rounded-md border-zinc-700 text-white'>
                 Anunciar e Vender
               </button>
@@ -39,15 +39,21 @@ const Header = () => {
 
           {
             anchorUserMenu 
-              ? <nav className='transition-all absolute top-14 right-32 w-54 py-2 px-1 opacity-0 rounded shadow-md bg-zinc-50'>
+              ? <nav className='transition-all absolute top-14 right-32 w-54 py-2 px-1 rounded shadow-md bg-zinc-50'>
                   <ul>
-                    <li className='p-2 transition-all delay-300 hover:bg-slate-100'>Meus anúncios</li>
-                    <li className='my-2 p-2 transition-all delay-300 hover:bg-slate-100'>Publicar novo anúncio</li>
+                    <Link href='../dashboard' passHref>
+                      <li className='p-2 transition-all delay-300 hover:bg-slate-100'>Meus anúncios</li>
+                    </Link>
+                    <Link href='../publish' passHref>
+                      <li className='my-2 p-2 transition-all delay-300 hover:bg-slate-100'>Publicar novo anúncio</li>
+                    </Link>
                     <hr />
-                    <li className='mt-1 p-2 transition-all delay-300 hover:bg-slate-100'>Sair</li>
+                    <Link href='../' passHref>
+                      <li className='mt-1 p-2 transition-all delay-300 hover:bg-slate-100'>Sair</li>
+                    </Link>
                   </ul>
                 </nav>
-            : null
+              : null
           }
         </ul>
       </nav>
